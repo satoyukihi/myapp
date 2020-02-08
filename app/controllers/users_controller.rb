9bedeb6068fff9b04c2f70a703246853c86fc287
sato <satoyu130@gmail.com>
2020-02-08 02:02:51 +0000

    newページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    #新規ユーザ登録ページ
+    #@user =User.newフォームからcreateにデーターを渡す
+  end
+  
+  def create
+    #データーを受け取る
+    #データーベースにセーブ
+    #フラッシュ
+    #リダイレクトホームページ
+    #失敗
+    #もう一度　render new
+    #
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  def index
+  end
+  
+  
+  
+  
+end
