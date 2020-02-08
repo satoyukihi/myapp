0a7f505110f4a2a9efe7f7fd75638210143bdd1f
sato <satoyu130@gmail.com>
2020-02-08 07:40:53 +0000

    usersテーブル作成、バリテーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  #名前空＋長さ50
+  validates :name, presence: true, length: { maximum: 50 }
+  #emailフォーマット定義
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #email空＋長さ50＋フォーマット+ユニーク（大文字）小文字区別なし
+  validates :email, presence:   true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
