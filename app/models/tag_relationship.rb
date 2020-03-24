99db6139157b617ccff4ceda18c5e7a3c1c50ef8
sato <satoyu130@gmail.com>
2020-03-24 16:17:10 +0900

    tag_relationshipモデルバリデーション追加

--- app/models/tag_relationship.rb
+++ app/models/tag_relationship.rb
@@ -1,4 +1,7 @@
 class TagRelationship < ApplicationRecord
   belongs_to :micropost
   belongs_to :tag
+  validates :micropost_id, presence: true
+  validates :tag_id, presence: true
+  validates :micropost_id, uniqueness: { scope: :tag_id }
 end
--- spec/models/tag_relationship_spec.rb
+++ spec/models/tag_relationship_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TagRelationship, type: :model do
-  let(:micropost) { FactoryBot.create(:micropost) }
   let(:tag_relationship) { FactoryBot.create(:tag_relationship) }
   context 'カラムのバリテーション' do
     before do
@@ -24,5 +23,13 @@ RSpec.describe TagRelationship, type: :model do
       tag_relationship.valid?
       expect(tag_relationship.errors[:tag]).to include('を入力してください')
     end
+    
+    it 'micropost_idとtag＿idがユニークでない場合無効な状態であること' do
+      tag = FactoryBot.build(:tag_relationship,
+                             micropost_id: tag_relationship.micropost_id,
+                             tag_id: tag_relationship.tag_id)
+      tag.valid?
+      expect(tag.errors[:micropost_id]).to include('はすでに存在します')
+  end
   end
 end
