4dd458b8cb12bc450123951b61e68d3c08f1b832
sato <satoyu130@gmail.com>
2020-03-28 15:44:08 +0900

    tagname長さ指定

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many  :tag_relationships, dependent: :destroy
   has_many  :microposts, through: :tag_relationships
 
-  validates :name, uniqueness: true,presence: true
+  validates :name, uniqueness: true,presence: true,length: { maximum: 10 }
 end
