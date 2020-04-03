b248dfa2fd430d645f78509f6d34bd3a70e3c604
sato <satoyu130@gmail.com>
2020-04-03 23:12:00 +0900

    follow通知作成

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -3,6 +3,7 @@ class FollowRelationshipsController < ApplicationController
   def create
     @user =User.find(params[:follow_relationship][:following_id])
     current_user.follow(@user)
+    @user.create_notification_follow!(current_user)
     respond_to do |format|
       format.html {redirect_back(fallback_location: root_url)}
       format.js 
