3a955813f7467e168fc9fc461e14c0948bd06e81
sato <satoyu130@gmail.com>
2020-04-04 15:17:49 +0900

    通知機能デザイン調整

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -8,12 +8,17 @@ module NotificationsHelper
       #notification.actionがfollowかlikeかcommentか
       case notification.action
         when "follow" then
-          tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"があなたをフォローしました"
+          tag.a(notification.visitor.name, href:user_path(@visitor))+"があなたをフォローしました"
         when "like" then
-          tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:micropost_path(notification.micropost_id), style:"font-weight: bold;")+"にいいねしました"
+          tag.a(notification.visitor.name, href:user_path(@visitor))+"が"+tag.a('あなたの投稿', href:micropost_path(notification.micropost_id))+"にいいねしました"
         when "comment" then
             @comment = Comment.find_by(id: @visitor_comment)&.content
-            tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:micropost_path(notification.micropost_id), style:"font-weight: bold;")+"にコメントしました"
+            tag.a(@visitor.name, href:user_path(@visitor))+"が"+tag.a('あなたの投稿', href:micropost_path(notification.micropost_id))+"にコメントしました"
       end
   end
+  
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
+  
 end
